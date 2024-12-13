72ee48c5687c7fa111e374b3f5b480ee38169b00
rosera <askrichardrose@gmail.com>
2024-12-13 20:54:00 +0000

    Add: Go fmt

--- glb/main.go
+++ glb/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"sort"
-  "log"
 )
 
 // Player represents an entry in the leaderboard
@@ -16,36 +16,35 @@ type Player struct {
 }
 
 var (
-  // leaderboards is a map that stores leaderboards for different games
-  leaderboards = map[string][]Player{}
-  port = 8080
+	// leaderboards is a map that stores leaderboards for different games
+	leaderboards = map[string][]Player{}
+	port         = 8080
 )
 
 func getGameLeaderboardEntries(game string, w http.ResponseWriter) {
 
- 	if game == "" {
- 		w.WriteHeader(http.StatusBadRequest)
- 		fmt.Fprintf(w, "Error: Game parameter is required")
- 		return
- 	}
- 	if players, ok := leaderboards[game]; ok {
- 		json.NewEncoder(w).Encode(players)
- 	} else {
- 		w.WriteHeader(http.StatusNotFound)
- 		fmt.Fprintf(w, "Leaderboard for %s not found", game)
- 	}
+	if game == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error: Game parameter is required")
+		return
+	}
+	if players, ok := leaderboards[game]; ok {
+		json.NewEncoder(w).Encode(players)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Leaderboard for %s not found", game)
+	}
 
-   return
+	return
 }
-
 
 func setGameLeaderboardScore(newPlayer Player, w http.ResponseWriter) {
 	leaderboard, ok := leaderboards[newPlayer.Game]
-  
+
 	if !ok {
 		leaderboard = []Player{}
 	}
-  
+
 	leaderboard = append(leaderboard, newPlayer)
 	// Sort based on score in descending order
 	sort.Slice(leaderboard, func(i, j int) bool {
@@ -71,31 +70,30 @@ func setGameLeaderBoardDelete(game string, w http.ResponseWriter) {
 func main() {
 	http.HandleFunc("/leaderboard", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
-		  case http.MethodGet:
-		  	game := r.URL.Query().Get("game")
-        getGameLeaderboardEntries(game, w)
+		case http.MethodGet:
+			game := r.URL.Query().Get("game")
+			getGameLeaderboardEntries(game, w)
 
-		  case http.MethodPost:
-		  	var newPlayer Player
-		  	err := json.NewDecoder(r.Body).Decode(&newPlayer)
-		  	if err != nil || newPlayer.Name == "" || newPlayer.Score == 0 || newPlayer.Game == "" {
-		  		w.WriteHeader(http.StatusBadRequest)
-		  		fmt.Fprintf(w, "Error: Game, Name and Score are required")
-		  		return
-		  	}
-        setGameLeaderboardScore(newPlayer, w)
+		case http.MethodPost:
+			var newPlayer Player
+			err := json.NewDecoder(r.Body).Decode(&newPlayer)
+			if err != nil || newPlayer.Name == "" || newPlayer.Score == 0 || newPlayer.Game == "" {
+				w.WriteHeader(http.StatusBadRequest)
+				fmt.Fprintf(w, "Error: Game, Name and Score are required")
+				return
+			}
+			setGameLeaderboardScore(newPlayer, w)
 
-		  case http.MethodDelete:
-		  	game := r.URL.Query().Get("game")
-        setGameLeaderBoardDelete(game,  w)
+		case http.MethodDelete:
+			game := r.URL.Query().Get("game")
+			setGameLeaderBoardDelete(game, w)
 
-
-		  default:
-		  	w.WriteHeader(http.StatusMethodNotAllowed)
-		  	fmt.Fprintf(w, "Method %s not allowed", r.Method)
+		default:
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintf(w, "Method %s not allowed", r.Method)
 		}
 	})
 
 	fmt.Println("Leaderboard API listening on port", port)
-  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
