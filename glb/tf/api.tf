549c35ef80b89ff5cd9b50e6ec38527e413e8b66
rosera <askrichardrose@gmail.com>
2024-12-16 14:40:52 +0000

    Add: Terraform codebase

---
+++ glb/tf/api.tf
@@ -0,0 +1,20 @@
+# Enable the Googleapi service
+
+resource "google_project_service" "tlf" {
+  ## https://developer.hashicorp.com/terraform/language/functions/toset
+  ## Removes duplicates
+  for_each = toset(var.api_services)
+
+  project = var.gcp_project_id 
+  service = each.key
+
+  disable_dependent_services = var.api_service_deny 
+}
+
+# Introduce a JIT delay for API enablement
+## Add a Delay before creating a Workbench instance
+resource "time_sleep" "wait_api_delay" {
+  depends_on = [ google_project_service.tlf ]
+  ## create_duration = "60s"
+  create_duration = var.api_create_duration 
+}
---
+++ glb/tf/vpc.tf
@@ -0,0 +1,28 @@
+resource "google_compute_network" "dev_network" {
+  name                    = var.vpc_network 
+  description             = var.vpc_network_description 
+  auto_create_subnetworks = false
+  project                 = var.gcp_project_id
+
+  # Give API time to be provisioned 
+  depends_on = [ time_sleep.wait_api_delay ]
+}
+
+resource "time_sleep" "wait_vpc_delay" {
+  create_duration = "60s"
+  depends_on      = [ google_compute_network.dev_network ]
+}
+
+# Reference:
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
+resource "google_compute_subnetwork" "dev_subnet" {
+  name          = var.vpc_subnet 
+  region        = var.gcp_region
+  ip_cidr_range = var.vpc_subnet_cidr 
+  network       = google_compute_network.dev_network.id
+  project       = var.gcp_project_id
+  private_ip_google_access = var.vpc_private_google_access
+
+  # Give network time to be created
+  depends_on = [ time_sleep.wait_vpc_delay ]
+}
